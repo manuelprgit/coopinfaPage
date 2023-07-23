851acde93979e1bd1c3a2a23feda470b46161d67
manuelDisacomp <manuel@disacomp.com>
2023-07-23 15:41:18 -0400

    Cambio de repositorio a Azure

--- public/js/main.js
+++ public/js/main.js
@@ -5,8 +5,7 @@ blogContent.addEventListener('click',e=>{
     if(e.target.closest('[data-blogid]')){
 
         let dataBlogId = e.target.closest('[data-blogid]').getAttribute('data-blogid');
-        localStorage.setItem('data-info-blog',dataBlogId);
-        getBloByID(dataBlogId);
+        localStorage.setItem('data-info-blog',dataBlogId); 
         window.location.href = `/getBlogById`;
     }
 
